4451bb96bae1ea1fe2df3eb697090e371b8e3fd9
Nicholas Voelker <volkoff223@gmail.com>
2023-01-09 16:07:12 -0500

    Deleted astro.conf

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "astro/config";
-
-import netlify from "@astrojs/netlify/functions";
-
-// https://astro.build/config
-export default defineConfig({
-  adapter: netlify(),
-  output: "server",
-});
