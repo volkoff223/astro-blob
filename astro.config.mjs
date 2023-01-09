d8bbeac50a7e67d3d04f91babbf092505cd048d2
Nicholas Voelker <volkoff223@gmail.com>
2023-01-09 16:02:52 -0500

    Deleted a line of code.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify/edge-functions";
 
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: netlify(),
-  output: "server"
+  output: "server",
 });
